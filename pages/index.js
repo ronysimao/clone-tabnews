ec9afaa00f92404dd47e9169bf10902066241dd6
ronysimao <ronysimao@gmail.com>
2024-01-14 22:40:50 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home(){
+    return <h1>Gzus Taralabarrébia Cova</h1>
+}
+
+//aqui basicamente eu estou dizendo ao React
+// que esta é a primeira função a ser chamada
+export default Home;
